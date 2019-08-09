1d3bf5901f52549f5b1ee0d27482e5f506445d73
Macmini01 <admin@Macmini01s-Mac-mini.local>
2019-08-09 18:34:28 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostList from './PostList';
+
+const App= () =>{
+
+    return (
+        <div className="ui containers">
+            <PostList />
+        </div>
+        );
+};
+
+export default App;
